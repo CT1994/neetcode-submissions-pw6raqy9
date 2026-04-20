3f6f2c34266d19aabeb29ddc8a24cf9dd777ece2
CT1994 <CT1994@users.noreply.github.com>
2026-04-20 19:42:49 +0100

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n) {
+        const cache = new Array(n-1).fill(null);
+
+        const dfs = (i) => {
+            if (i > n) {
+                return 0
+            }
+
+            if (i === n) {
+                return 1
+            }
+
+            if (cache[i]) {
+                return cache[i]
+            }
+
+            cache[i] = dfs(i + 1) + dfs(i + 2)
+            return cache[i]
+        }
+
+        return dfs(0)
+    }
+}
