fd36a910341b17f8933e727797e57f55e87cdf4e
CT1994 <CT1994@users.noreply.github.com>
2026-05-03 20:17:19 +0100

    Add: ipo - submission-2

---
+++ Data Structures & Algorithms/ipo/submission-2.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number} k
+     * @param {number} w
+     * @param {number[]} profits
+     * @param {number[]} capital
+     * @return {number}
+     */
+    findMaximizedCapital(k, w, profits, capital) {
+        let res = w;
+        const maxProfits = new MaxPriorityQueue(val => val.profit);
+        const minCapital = new MinPriorityQueue(val => val.capital);
+
+        for (let i = 0; i < profits.length; i++) {
+            minCapital.enqueue({ capital: capital[i], profit: profits[i] });
+        }
+
+        for (let i = 0; i < k; i++) {
+            while (!minCapital.isEmpty() && minCapital.front().capital <= res) {
+                maxProfits.enqueue(minCapital.dequeue())
+            }
+
+            if (!maxProfits.isEmpty()) {
+                res += maxProfits.dequeue().profit
+            }
+        }
+
+        return res
+    }
+}
