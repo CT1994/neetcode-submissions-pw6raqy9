1724db55372539a61a71ba05dbd73cec0c3b86ba
CT1994 <CT1994@users.noreply.github.com>
2026-05-05 18:48:29 +0100

    Add: combinations-of-a-phone-number - submission-1

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-1.js
@@ -0,0 +1,44 @@
+class Solution {
+    /**
+     * @param {string} digits
+     * @return {string[]}
+     */
+    letterCombinations(digits) {
+        if (digits === "") {
+            return [];
+        }
+
+        const digitToChar = {
+            2: ["a", "b", "c"],
+            3: ["d", "e", "f"],
+            4: ["g", "h", "i"],
+            5: ["j", "k", "l"],
+            6: ["m", "n", "o"],
+            7: ["p", "q", "r", "s"],
+            8: ["t", "u", "v"],
+            9: ["w", "x", "y", "z"],
+        };
+
+        const combinations = [];
+        const curCombination = [];
+
+        const helper = (i) => {
+            if (curCombination.length === digits.length) {
+                combinations.push(curCombination.join(""));
+                return;
+            }
+
+            for (let j = i; j < digits.length; j++) {
+                const chars = digitToChar[digits[j]];
+                for (const char of chars) {
+                    curCombination.push(char);
+                    helper(j + 1);
+                    curCombination.pop();
+                }
+            }
+        };
+
+        helper(0);
+        return combinations;
+    }
+}
