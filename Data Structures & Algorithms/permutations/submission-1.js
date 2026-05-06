29e720570a5461cd0e645e19ad0b9a902d0bbad0
CT1994 <CT1994@users.noreply.github.com>
2026-05-06 10:52:13 +0100

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    permute(nums) {
+        let perms = [[]]
+
+        for (const n of nums) {
+            const nextPerms = [];
+            for (const perm of perms) {
+                for (let i = 0; i <= perm.length; i++) {
+                    const pCopy = [...perm];
+                    pCopy.splice(i, 0, n);
+                    nextPerms.push(pCopy)
+                }
+            }
+
+            perms = nextPerms
+        }
+
+        return perms
+    }
+}
