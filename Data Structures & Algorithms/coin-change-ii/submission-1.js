45c461eb2b7f9769abb5d9ddca0e7bdd2f308977
CT1994 <CT1994@users.noreply.github.com>
2026-05-12 13:30:21 +0100

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number} amount
+     * @param {number[]} coins
+     * @return {number}
+     */
+    change(amount, coins) {
+        const dp = new Array(amount + 1).fill(0);
+        dp[0] = 1;
+
+        for (const coin of coins) {
+            for (let i = coin; i <= amount; i++) {
+                if (dp[i - coin] >= 0) {
+                    dp[i] += dp[i - coin];
+                }
+            }
+        }
+
+        return dp[amount];
+    }
+}
